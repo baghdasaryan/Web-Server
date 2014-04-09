7088f560433f7688c1342b6dba4679377b5b0fd1
CS118 <cs118@ubuntu.(none)>
2014-04-09 03:27:12 -0700

    first commit

---
+++ server.h
@@ -0,0 +1,27 @@
+// ######################################### //
+// ######################################### //
+// ####                                 #### //
+// #### Program: C++ Web-Server         #### //
+// #### Language: C++                   #### //
+// #### Author: Georgi Baghdasaryan     #### //
+// #### Email: baghdasaryan@ucla.edu    #### //
+// ####                                 #### //
+// ######################################### //
+// ######################################### //
+
+
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+class Server {
+    public:
+        Server();
+        ~Server();
+
+        void getFile();
+
+    private:
+
+}
+
+#endif  // __SERVER_H__
